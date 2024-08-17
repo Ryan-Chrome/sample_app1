337c27e859e3d6a47434c3b9183e3482fe3bffe0
ryan_shadow <ryan_shadow@example.com>
2024-08-18 01:48:32 +0900

    改行削除

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,7 +1,6 @@
 require "test_helper"
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  
   test "layout links" do
     get root_path
     assert_template "static_pages/home"
@@ -10,5 +9,4 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
   end
-
 end
